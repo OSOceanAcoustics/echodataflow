7b7b0c3475a2261489dd8d610eb2127e904da05d
Sohambutala <sohambutala@yahoo.in>
2024-05-24 16:00:13 -0700

    Added In-memory testing in pytest

--- echodataflow/tests/flow_tests/flow_test.py
+++ echodataflow/tests/flow_tests/flow_test.py
@@ -2,15 +2,41 @@ from pathlib import Path
 from echodataflow.stages.echodataflow import echodataflow_start
 import pytest
 
+
 @pytest.fixture
 def dataset_config():
     return Path("datastore.yaml").resolve()
 
-@pytest.fixture(params=[Path("./MVBS_pipeline.yaml").resolve(), Path("./mask_pipeline.yaml").resolve(), Path("./TS_pipeline.yaml").resolve()])
+
+@pytest.fixture(
+    params=[
+        Path("./MVBS_pipeline.yaml").resolve(),
+        Path("./mask_pipeline.yaml").resolve(),
+        Path("./TS_pipeline.yaml").resolve(),
+    ]
+)
 def pipeline_config(request):
     return request.param
 
+
+@pytest.fixture(
+    params=[Path("./MVBS_pipeline_memory.yaml").resolve()]
+)  # , Path("./mask_pipeline_memory.yaml").resolve(), Path("./TS_pipeline_memory.yaml").resolve()
+def pipeline_config_memory(request):
+    return request.param
+
+
 def test_pipeline(dataset_config, pipeline_config):
     options = {"storage_options_override": False}
-    data = echodataflow_start(dataset_config=dataset_config, pipeline_config=pipeline_config, options=options)
+    data = echodataflow_start(
+        dataset_config=dataset_config, pipeline_config=pipeline_config, options=options
+    )
+    assert data is not None, "Expected data to be non-None"
+
+
+def test_pipeline_memory(dataset_config, pipeline_config_memory):
+    options = {"storage_options_override": False}
+    data = echodataflow_start(
+        dataset_config=dataset_config, pipeline_config=pipeline_config_memory, options=options
+    )
     assert data is not None, "Expected data to be non-None"
