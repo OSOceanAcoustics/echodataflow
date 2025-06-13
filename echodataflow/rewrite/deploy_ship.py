2a1c83d621285be8da52497383bddabb0304c3e4
Wu-Jung Lee <leewujung@gmail.com>
2025-06-13 10:53:40 -0700

    do everything in UTC

--- echodataflow/rewrite/deploy_ship.py
+++ echodataflow/rewrite/deploy_ship.py
@@ -32,10 +32,12 @@ if __name__ == "__main__":
     if init_dict["flow_start_time"] is None:
         curr_time_offset = datetime.timedelta(seconds=0)
     else:
+        # Do all calculations in UTC
         curr_time_offset = (
-            datetime.datetime.now()
-            - datetime.datetime.strptime(
-                init_dict["flow_start_time"], "%Y%m%dT%H%M%S"
+            datetime.datetime.now(datetime.timezone.utc)
+            - (
+                datetime.datetime.fromisoformat(init_dict["flow_start_time"])
+                .astimezone(datetime.timezone.utc)
             )
         )
 
@@ -57,14 +59,14 @@ if __name__ == "__main__":
         #     name="copy-raw",
         #     # cron=f"*/{interval_dict["copy_raw"]} * * * *",
         # ),
-        flow_raw2Sv.from_source(
-            source=str(Path(__file__).parent),
-            entrypoint="flows_acoustics.py:flow_raw2Sv",
-        ).to_deployment(
-            name="raw2Sv_test",
-            parameters=config["raw2Sv"],
-            # cron=f"*/{interval_dict["raw2Sv"]} * * * *",
-        ),
+        # flow_raw2Sv.from_source(
+        #     source=str(Path(__file__).parent),
+        #     entrypoint="flows_acoustics.py:flow_raw2Sv",
+        # ).to_deployment(
+        #     name="raw2Sv_test",
+        #     parameters=config["raw2Sv"],
+        #     # cron=f"*/{interval_dict["raw2Sv"]} * * * *",
+        # ),
         flow_create_MVBS.from_source(
             source=str(Path(__file__).parent),
             entrypoint="flows_acoustics.py:flow_create_MVBS",
@@ -73,14 +75,14 @@ if __name__ == "__main__":
             parameters=config["create_MVBS"],
             # cron=f"*/{interval_dict["create_MVBS"]} * * * *",
         ),
-        flow_predict_hake.from_source(
-            source=str(Path(__file__).parent),
-            entrypoint="flows_acoustics.py:flow_predict_hake",
-        ).to_deployment(
-            name="predict-hake_test",
-            parameters=config["predict_hake"],
-            # cron=f"*/{interval_dict["predict_hake"]} * * * *",
-        ),
+        # flow_predict_hake.from_source(
+        #     source=str(Path(__file__).parent),
+        #     entrypoint="flows_acoustics.py:flow_predict_hake",
+        # ).to_deployment(
+        #     name="predict-hake_test",
+        #     parameters=config["predict_hake"],
+        #     # cron=f"*/{interval_dict["predict_hake"]} * * * *",
+        # ),
         work_pool_name="local",
     )
     flow_file_upload.from_source(
@@ -92,12 +94,12 @@ if __name__ == "__main__":
         work_pool_name="local",
         cron=f"*/{interval_dict["file_upload_acoustics"]} * * * *",
     ).apply()
-    flow_file_upload.from_source(
-        source=str(Path(__file__).parent),
-        entrypoint="helpers.py:flow_file_upload",
-    ).to_deployment(
-        name="file-upload-trawl_test",
-        parameters=config["file_upload_trawl"],
-        cron=f"*/{interval_dict["file_upload_trawl"]} * * * *",
-        work_pool_name="local",
-    ).apply()
+    # flow_file_upload.from_source(
+    #     source=str(Path(__file__).parent),
+    #     entrypoint="helpers.py:flow_file_upload",
+    # ).to_deployment(
+    #     name="file-upload-trawl_test",
+    #     parameters=config["file_upload_trawl"],
+    #     cron=f"*/{interval_dict["file_upload_trawl"]} * * * *",
+    #     work_pool_name="local",
+    # ).apply()
