5a96a90cb36182f4f8e88c82e8140bf3b549834b
leewujung <leewujung@gmail.com>
2025-09-08 11:23:31 -0700

    add curren ship deployment and config

--- echodataflow/rewrite/deploy_ship.py
+++ echodataflow/rewrite/deploy_ship.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
             source=str(Path(__file__).parent),
             entrypoint="flows_acoustics.py:flow_raw2Sv",
         ).to_deployment(
-            name="raw2Sv_leg2",
+            name="raw2Sv",
             parameters=config["raw2Sv"],
             cron=f"*/{interval_dict["raw2Sv"]} * * * *",
         ),
@@ -72,7 +72,7 @@ if __name__ == "__main__":
             source=str(Path(__file__).parent),
             entrypoint="flows_acoustics.py:flow_create_MVBS",
         ).to_deployment(
-            name="create-MVBS_leg2",
+            name="create-MVBS",
             parameters=config["create_MVBS"],
             cron=f"3-59/{interval_dict["create_MVBS"]} * * * *",
         ),
@@ -80,7 +80,7 @@ if __name__ == "__main__":
             source=str(Path(__file__).parent),
             entrypoint="flows_acoustics.py:flow_predict_hake",
         ).to_deployment(
-            name="predict-hake_leg2",
+            name="predict-hake",
             parameters=config["predict_hake"],
             cron=f"*/{interval_dict["predict_hake"]} * * * *",
         ),
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         source=str(Path(__file__).parent),
         entrypoint="helpers.py:flow_file_upload",
     ).to_deployment(
-        name="file-upload-acoustics_leg2",
+        name="file-upload-acoustics",
         parameters=config["file_upload_acoustics"],
         work_pool_name="local",
         cron=f"*/{interval_dict["file_upload_acoustics"]} * * * *",
@@ -99,8 +99,8 @@ if __name__ == "__main__":
         source=str(Path(__file__).parent),
         entrypoint="helpers.py:flow_file_upload",
     ).to_deployment(
-        name="file-upload-trawl_20250902",
+        name="file-upload-trawl",
         parameters=config["file_upload_trawl"],
-        cron=f"*/{interval_dict["file_upload_trawl"]} * * * *",
+        cron=f"3-59/{interval_dict["file_upload_trawl"]} * * * *",
         work_pool_name="local",
     ).apply()
